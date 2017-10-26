1fc93f7709758da4c3a1cde5c4a2117c89ce6347
Alain Otjens <alain@waag.org>
2017-10-26 21:22:49 +0200

    modify beta notification

--- _site/geothesaurus/application/views/header.php
+++ _site/geothesaurus/application/views/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>ErfGeo | Nederlandse topografie door de tijd (beta) </title>
+<title>ErfGeo | Nederlandse topografie door de tijd </title>
 
 <meta name="author" content="Waag Society">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
